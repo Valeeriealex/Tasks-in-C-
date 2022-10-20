436403168f90a2eda4dcabe3240ec191ca78917b
Vlerie <Valerie.Basaly@mail.ru>
2022-10-20 18:17:36 +0300

    Редактировнаие задачи 28, добавление задач 34 36

--- task28/Program.cs
+++ task28/Program.cs
@@ -1,8 +1,6 @@
 ﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 //1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
 
-Console.Write("Введите ряд чисел, разделенных запятой: ");
-
 int[] FillArray(int num)
 {
     int[] array = new int [num];
@@ -15,7 +13,7 @@ int[] FillArray(int num)
 }
 void PrintArray (int[] arr)
 {
-    for (int i=  0; i < arr.Length; i++)
+    for (int i =  0; i < arr.Length; i++)
     {
         Console.Write($"{arr[i]}, ");
     }
--- task34/Program.cs
+++ task34/Program.cs
@@ -2,7 +2,7 @@
 //Напишите программу, которая покажет количество чётных чисел в массиве.
 //[345, 897, 568, 234] -> 2
 
-Console.WriteLine("Введите размер массива");
+Console.WriteLine("Введите размер массива ");
 int size = Convert.ToInt32(Console.ReadLine());
 int[] numbers = new int[size];
 FillArrayRandomNumbers(numbers);
