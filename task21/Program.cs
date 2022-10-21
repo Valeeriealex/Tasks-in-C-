5329936a240be78102da091ef1fbae4ba9a82730
Vlerie <Valerie.Basaly@mail.ru>
2022-10-21 22:45:55 +0300

    Исправил задачи 21 23

--- task21/Program.cs
+++ task21/Program.cs
@@ -1,29 +1,33 @@
-﻿// Задача 21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+﻿// Задача 21 Напишите программу, которая принимает на вход координаты двух точек 
+//и находит расстояние между ними в 3D пространстве.
 //A (3,6,8); B (2,1,-7), -> 16.58
 //A (7,-5, 0); B (1,-1,9) -> 11.53
 
-int x1 = Coordinate("x", "A");
-int y1 = Coordinate("y", "A");
-int z1 = Coordinate("z", "A");
-int x2 = Coordinate("x", "B");
-int y2 = Coordinate("y", "B");
-int z2 = Coordinate("z", "B");
+//Math.Sqrt(56);
+//Math.Pow (2,10);
 
-int Coordinate(string coorName, string pointName)
+//double d = 5.09997676;
+//double resD = Math.Round (d, 2);
+//MidpointRounding.ToZero;
+//System.Console.WriteLine (resD);
+
+Console.WriteLine ("Введите координаты точки x точки A");
+int xa = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите координаты точки y точки A");
+int ya = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите координаты точки z точки A");
+int za = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите координаты точки x точки B");
+int xb = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите координаты точки y точки B");
+int yb = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите координаты точки z точки B");
+int zb = Convert.ToInt32(Console.ReadLine());
+
+double distance = Math.Round(Distance (xa, ya, za, xb, yb, zb), 2, MidpointRounding.ToZero);
+Console.WriteLine($"Расстояние от A до B {distance}");
+
+double Distance (int x1, int y1, int z1, int x2, int y2, int z2)
 {
-    Console.WriteLine($"Введите координату {coorName} точки {pointName}: ");
-    return Convert.ToInt16(Console.ReadLine());
+  return Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2)); 
 }
-
-double Decision(double x1, double x2, 
-                double y1, double y2, 
-                double z1, double z2)
-{
-  return Math.Sqrt(Math.Pow((x2-x1), 2) + 
-                   Math.Pow((y2-y1), 2) + 
-                   Math.Pow((z2-z1), 2));
-}
-
-double segmentLength =  Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 );
-
-Console.WriteLine($"Длина отрезка  {segmentLength}");
--- task23/Program.cs
+++ task23/Program.cs
@@ -1,4 +1,5 @@
-﻿// Задача 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+﻿// Задача 23 Напишите программу, которая принимает на вход число (N) 
+//и выдаёт таблицу кубов чисел от 1 до N.
 //5 ->
 //1 | 1
 //2 | 8
@@ -6,29 +7,33 @@
 //4 | 64
 //5 | 125
 
+//Console.WriteLine("Введите число: ");
+//int num = Convert.ToInt32(Console.ReadLine());
+//int count = 1;
+
+//while (count <= num)
+//{
+  //Console.WriteLine(Cube(count));
+  //count++;
+//}
+
+//string Cube(int number)
+//{
+ // return ($"{number} - > {number*number*number}");
+//}
+
 Console.WriteLine("Введите число: ");
-int cube = Convert.ToInt32(Console.ReadLine());
+int number = Convert.ToInt32(Console.ReadLine());
 
-int[] arry = new int[cube+1];
-Cube(arry);
-PrintArry(arry);
-
-void Cube(int[] cube)
+Cube (number);
+void Cube (int num)
 {
-  int counter = 1;
-  int length = cube.Length;
-  while (counter <  length){
-    cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
-    counter++;
+  int count = 1;
+
+  while (count <= num)
+  {
+    int cube = count*count*count;
+    Console.WriteLine ($"{count} {cube}");
+    count++;
   }
 }
-
-void PrintArry(int[] coll)
-{
-  int count = coll.Length;
-  int index = 1;
-  while(index < count){
-    Console.Write(coll[index]+ " ");
-    index++;
-  }
-} 
