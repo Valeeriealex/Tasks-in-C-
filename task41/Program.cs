df063051bd1c76f54fdb078b450da26340523834
Vlerie <Valerie.Basaly@mail.ru>
2022-10-25 01:55:04 +0300

    Добавил задачи 41 43

---
+++ task41/Program.cs
@@ -0,0 +1,43 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//-1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите числа: ");
+int[] numbers = StringToNum(Console.ReadLine());
+PrintArray(numbers);
+int sum = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > 0)
+    {
+        sum++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"Количество чисел больше 0 = {sum}");
+
+
+//Метод "Строка чисел"
+int[] StringToNum(string input)
+{
+ int count = 0;
+ for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+    }
+
+//Метод "Вывод на печать"
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.Write("]");
+}
